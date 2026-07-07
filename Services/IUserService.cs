b1c0ad313afc514493b7f96ec028863d3c84d3c4
OvsyannikovID <OvsyannikovID@IN-001705>
2026-07-07 17:14:28 +0500

    добавил пару файлов

---
+++ Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using MongoAPI.Models;
+
+namespace MongoAPI.Services
+{
+    public interface IUserService
+    {
+        Task<User?> GetByLoginAsync(string login);
+        Task<User?> CreateAsync(User user);
+        Task<List<User>> GetALLAsync();
+        Task<User> GetByLogin(string login);
+    }
+}
