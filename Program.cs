96bd8ea8a441c1293bc169cc0258f3191d5f888e
OvsyannikovID <OvsyannikovID@IN-001705>
2026-07-09 09:40:49 +0500

    2

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ namespace MongoAPI
             //builder.Services.AddAuthorization();
             var connectionString = builder.Configuration["Database:ConnectionString"];
             Database database = new Mongo(connectionString);
-
+            builder.Services.AddScoped<JwtService>();
             builder.Services.AddSingleton(database);
 
 
